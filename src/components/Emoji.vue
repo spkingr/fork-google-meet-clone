d3e90d60bc6ee6c113ee2053ffe76681f848fd3b
seemrcola <seemrcola@163.com>
2023-08-13 00:39:00 +0800

    feat: emoji

--- src/components/Emoji.vue
+++ src/components/Emoji.vue
@@ -1,9 +1,33 @@
 <script setup lang='ts'>
+import data from 'emoji-mart-vue-fast/data/all.json'
+import 'emoji-mart-vue-fast/css/emoji-mart.css'
 
+// Vue 3, import components from `/src`:
+// @ts-expect-error
+import { EmojiIndex, Picker } from 'emoji-mart-vue-fast/src'
+
+type EmojiType = 'apple' | 'google' | 'twitter' | 'messenger'
+
+defineProps({
+  emojiType: {
+    type: String as PropType<EmojiType>,
+    default: 'apple',
+  },
+})
+
+const emits = defineEmits(['select'])
+
+// Create emoji data index.
+// We can change it (for example, filter by category) before passing to the component.
+const emojiIndex = new EmojiIndex(data)
+
+function showEmoji(emoji: any) {
+  emits('select', emoji.native)
+}
 </script>
 
 <template>
-  <div h="360px" w="480px">
-    hello world
+  <div>
+    <Picker :data="emojiIndex" :set="emojiType" @select="showEmoji" />
   </div>
 </template>
--- src/pages/meet/components/Chat.vue
+++ src/pages/meet/components/Chat.vue
@@ -1,7 +1,9 @@
 <script setup lang='ts'>
+import 'lu2/theme/edge/css/common/ui/Tips.css'
 import dayjs from 'dayjs'
 import { MessageTypeEnum } from '../types'
 import type { Message, User } from '../types'
+import { useUserStore } from '~/store/useUser'
 
 const props = defineProps<{
   msgList: Message[]
@@ -11,6 +13,8 @@ const props = defineProps<{
 const emits = defineEmits<{
   (e: 'sendMessage', msg: Message): void
 }>()
+
+const userStore = useUserStore()
 
 const message = ref('')
 
@@ -26,11 +30,28 @@ function sendMessage() {
   emits('sendMessage', msg)
   message.value = ''
 }
+
+const emojiVisible = ref(false)
+function showEmoji() {
+  emojiVisible.value = !emojiVisible.value
+}
+function selectEmoji(emoji: string) {
+  message.value += emoji
+}
+function greet() {
+  message.value = `${userStore.user.name} => 👋 👋 👋`
+}
+function clearMessage() {
+  message.value = ''
+}
+function showTime() {
+  message.value = dayjs(new Date()).format('HH:mm:ss')
+}
 </script>
 
 <template>
   <div w-full h-full>
-    <div w-full h="[calc(100%-100px)]" px-2 py-2 box-border>
+    <div w-full h="[calc(100%-130px)]" px-2 py-2 box-border>
       <div
         v-for="(msg, index) of props.msgList" :key="index"
         py-2 px-4 rounded-2 bg-light mb-1
@@ -47,21 +68,39 @@ function sendMessage() {
         </div>
       </div>
     </div>
-    <div w-full h="100px" px-2 py-4 bg-light relative>
-      <textarea
-        v-model="message"
-        class="ui-textarea !pr-10"
-        placeholder="......"
-        maxlength="80" resize-none
-        w-full h-full
-      />
-      <div
-        i-bx:bxs-paper-plane
-        absolute right-5 text-2xl top="50%" translate-y="-50%"
-        transition-400 cursor-pointer
-        hover="bg-teal-600"
-        @click="sendMessage"
-      />
+    <div h="130px" relative bg-light box-border>
+      <div h="30px" flex items-center>
+        <div mx-2 text-xl class="ui-tips" title="emoji" @click="showEmoji">
+          <div text-teal-6 i-solar:emoji-funny-circle-bold />
+        </div>
+        <div mx-2 class="ui-tips" title="greet" @click="greet">
+          <div text-teal-6 i-solar:hand-shake-broken />
+        </div>
+        <div mx-2 class="ui-tips" title="clear" @click="clearMessage">
+          <div text-teal-6 i-material-symbols:arrow-left-alt />
+        </div>
+        <div mx-2 class="ui-tips" title="time" @click="showTime">
+          <div text-teal-6 i-mdi:clock-outline />
+        </div>
+
+        <Emoji v-if="emojiVisible" absolute bottom="130px" left="4px" @select="selectEmoji" />
+      </div>
+      <div w-full h="90px" p="x-2 y-0" relative>
+        <textarea
+          v-model="message"
+          class="ui-textarea !pr-10"
+          placeholder="......"
+          maxlength="80" resize-none
+          w-full h-full
+        />
+        <div
+          i-bx:bxs-paper-plane
+          absolute right-5 text-2xl top="50%" translate-y="-50%"
+          transition-400 cursor-pointer
+          hover="bg-teal-600"
+          @click="sendMessage"
+        />
+      </div>
     </div>
   </div>
 </template>
